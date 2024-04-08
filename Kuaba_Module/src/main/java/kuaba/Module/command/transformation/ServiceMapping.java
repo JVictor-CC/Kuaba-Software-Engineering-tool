bf23793b2cba56b220735266d5d5d4a8633061ee
João Victor de Souza <jvcunha1221@hotmail.com>
2024-04-08 17:52:25 -0300

    refactor: Remove comments and change variable names

--- Kuaba_Module/src/main/java/kuaba/Module/command/transformation/ServiceMapping.java
+++ Kuaba_Module/src/main/java/kuaba/Module/command/transformation/ServiceMapping.java
@@ -1,6 +1,5 @@
 package kuaba.Module.command.transformation;
 
-import org.eclipse.emf.common.util.EList;
 import org.modelio.api.modelio.model.IModelingSession;
 import org.modelio.api.modelio.model.ITransaction;
 import org.modelio.api.module.IModule;
@@ -14,31 +13,25 @@ public class ServiceMapping extends GeneralMapping{
 	
     public void mapService(IModelingSession session, IModule module, org.modelio.metamodel.uml.statik.Package target, Class element) { 
 		Stereotype javaClassStereotype = session.getMetamodelExtensions().getStereotype("JavaDesigner", "JavaClass", module.getModuleContext().getModelioServices().getMetamodelService().getMetamodel().getMClass(Class.class));
-		Stereotype javaInterfaceStereotype = session.getMetamodelExtensions().getStereotype("JavaDesigner", "JavaInterface", module.getModuleContext().getModelioServices().getMetamodelService().getMetamodel().getMClass(Class.class));
-		
-        try (ITransaction t = session.createTransaction("Process Service")) {
+		Stereotype javaInterfaceStereotype = session.getMetamodelExtensions().getStereotype("JavaDesigner", "JavaInterface", module.getModuleContext().getModelioServices().getMetamodelService().getMetamodel().getMClass(Interface.class));
+        try (ITransaction transaction = session.createTransaction("Process Service")) {
         	
-        	// Cria uma classe estereootipada com "JavaClass" dentro do PSM, juntamente com uma interface que a classe implementa        	
         	Class myClass = session.getModel().createClass(element.getName(), target, javaClassStereotype);       	
-        	Interface myInterface = session.getModel().createInterface("I" + element.getName(), target, javaInterfaceStereotype);      	
+        	Interface myInterface = session.getModel().createInterface("I" + element.getName(), target, javaInterfaceStereotype);       	
             InterfaceRealization realization = session.getModel().createInterfaceRealization();
             
-            // Define a relação de quem implementa e quem está sendo implementado         
             realization.setImplementer(myClass);                
             realization.setImplemented(myInterface);
             
-            // Processa suas operações            
             processOperations(session, module, myClass, element);                   
             processOperations(session, module, myInterface, element);
             
             for (Operation operation : myInterface.getOwnedOperation()) {
-                operation.setIsAbstract(true);
-            }
-            
-            t.commit();               
+                operation.setIsAbstract(true);                
+            }   
+            transaction.commit();               
         } catch (Exception e) {
             module.getModuleContext().getLogService().error(e);
         }
 	}
-    
 }
